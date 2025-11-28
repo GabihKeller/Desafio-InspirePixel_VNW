f2d55a6fcb987e4d857811c54c728a7b568dca64
GabihKeller <gabihsk99@gmail.com>
2025-11-28 12:56:45 -0300

    Primeiro commit

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { Icon } from "@iconify/vue";
+import logo from "../assets/logo.png"
+
+</script>
+
+<template>
+<header>
+    <img :src="logo" alt="Imagem da logo marca">
+   <nav>
+    <li>
+        <a href="">
+            <Icon class="search-icon" icon="material-symbols:search" />
+            <!-- <Icon icon="coleção:nomeDoIconeEmIngles" -->
+        </a>
+    </li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+        <a href="">
+            <Icon class="user-icon" icon="ph:user"/>
+        </a>
+   </nav>
+</header>
+</template>
+
+<style scoped lang="scss">
+
+</style>
